c787c4df94af27c7a2e3d2323fcb25f9cb936ddb
AdlerCastro <adlercastro01@gmail.com>
2024-07-25 17:37:31 -0300

    :lipstick: feat: Posicionamento da sidebar Ajuste de fonte Poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
+import SideBar from "@/components/organisms/SideBar";
 
-const poppins = Poppins({ 
+const poppins = Poppins({
   subsets: ["latin"],
-  weight: "800"
+  weight: ['100', "200", "300", "400", "500", "600", "700", "800", "900"]
 });
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <main className="">
+        <SideBar />
+        <main className="w-full h-full">
           {children}
         </main>
       </body>
