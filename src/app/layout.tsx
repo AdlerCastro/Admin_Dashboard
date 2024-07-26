e46c0d0690cc6a44434b586b39dbde20d7a08a7d
AdlerCastro <adlercastro01@gmail.com>
2024-07-26 05:38:19 -0300

    :construction: Desenvolver o context api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={poppins.className}>
         <SideBar />
-        <main className="w-full h-full">
+        <main>
           {children}
         </main>
       </body>
