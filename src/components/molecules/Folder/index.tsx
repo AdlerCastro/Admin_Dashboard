a5e8dc906d7557be4c2907cad4dd278c614b812e
AdlerCastro <adlercastro01@gmail.com>
2024-07-26 14:23:42 -0300

    :lipstick: style: Animação das setas

--- src/components/molecules/Folder/index.tsx
+++ src/components/molecules/Folder/index.tsx
@@ -28,12 +28,14 @@ export default function Folder() {
                 <input type="checkbox" name="checkbox" id="checkbox-geral" className="hidden" onClick={toggleGeral} />
                 <label htmlFor="checkbox-geral" className='font-semibold text-base cursor-pointer flex flex-row gap-2 items-center'>
                     <span className={` w-5 border-solid items-center
+                    transition-all duration-300
                     before:w-[13px] before:mt-1 before:h-[3px] before:relative before:block dark:before:bg-white before:bg-black
                     after:w-[13px] after:mt-1 after:h-[3px] after:relative after:block dark:after:bg-white after:bg-black
-                        ${openGeral ? ' after:rotate-[225deg] before:rotate-[45deg]' : 'after:rotate-[-45deg] before:rotate-[45deg]'}`
+                    after:rotate-[-45deg] before:rotate-[45deg]
+                        ${openGeral ? 'rotate-90' : 'rotate-0'}`
                     }></span> Geral
                 </label>
-                <ul className={` transition-all duration-300 list-none overflow-hidden ${openGeral ? 'h-[140px]' : 'h-0'}`}>
+                <ul className={`transition-all duration-300 list-none overflow-hidden ${openGeral ? 'h-[140px]' : 'h-0'}`}>
                     <div className='transition-all duration-300 list-none flex flex-col gap-y-2 p-1 bg-transparent text-center '>
                         <li><Button className='font-normal'>aaa</Button ></li>
                         <li><Button className='font-normal'>bbb</Button ></li>
@@ -45,10 +47,12 @@ export default function Folder() {
             <nav className='items-center'>
                 <input type="checkbox" name="checkbox" id="checkbox-ferramentas" className="hidden" onClick={toggleFerramentas} />
                 <label htmlFor="checkbox-ferramentas" className='font-semibold text-base cursor-pointer flex flex-row gap-2 items-center'>
-                    <span className={` w-5 border-solid items-center
+                <span className={` w-5 border-solid items-center
+                    transition-all duration-300
                     before:w-[13px] before:mt-1 before:h-[3px] before:relative before:block dark:before:bg-white before:bg-black
                     after:w-[13px] after:mt-1 after:h-[3px] after:relative after:block dark:after:bg-white after:bg-black
-                        ${openFerramentas ? ' after:rotate-[225deg] before:rotate-[45deg]' : 'after:rotate-[-45deg] before:rotate-[45deg]'}`
+                    after:rotate-[-45deg] before:rotate-[45deg]
+                        ${openFerramentas ? 'rotate-90' : 'rotate-0'}`
                     }></span> Ferramentas
                 </label>
                 <ul className={` transition-all duration-300 list-none overflow-hidden ${openFerramentas ? 'h-[140px]' : 'h-0'}`}>
@@ -63,10 +67,12 @@ export default function Folder() {
             <nav className='items-center'>
                 <input type="checkbox" name="checkbox" id="checkbox-opcional" className="hidden" onClick={toggleOpcional} />
                 <label htmlFor="checkbox-opcional" className='font-semibold text-base cursor-pointer flex flex-row gap-2 items-center'>
-                    <span className={` w-5 border-solid items-center
+                <span className={` w-5 border-solid items-center
+                    transition-all duration-300
                     before:w-[13px] before:mt-1 before:h-[3px] before:relative before:block dark:before:bg-white before:bg-black
                     after:w-[13px] after:mt-1 after:h-[3px] after:relative after:block dark:after:bg-white after:bg-black
-                        ${openOpcional ? ' after:rotate-[225deg] before:rotate-[45deg]' : 'after:rotate-[-45deg] before:rotate-[45deg]'}`
+                    after:rotate-[-45deg] before:rotate-[45deg]
+                        ${openOpcional ? 'rotate-90' : 'rotate-0'}`
                     }></span> Opcional
                 </label>
                 <ul className={` transition-all duration-300 list-none overflow-hidden ${openOpcional ? 'h-[140px]' : 'h-0'}`}>
