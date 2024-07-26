17d5298c3c82787f2079f623767c6ce62a121231
AdlerCastro <adlercastro01@gmail.com>
2024-07-26 05:39:36 -0300

    :construction: Desenvolvendo a side bar

--- src/components/organisms/SideBar/index.tsx
+++ src/components/organisms/SideBar/index.tsx
@@ -1,10 +1,9 @@
-import Button from '@/components/atoms/Button'
 import Folder from '@/components/molecules/Folder'
 import React from 'react'
 
 export default function SideBar() {
     return (
-        <aside className='bg-zinc-100 dark:bg-zinc-800 flex flex-1 flex-col gap-3 p-4'>
+        <aside className=' bg-zinc-100 dark:bg-zinc-800 flex flex-col px-6 py-4 rounded-e-md overflow-auto'>
             <h1 className='font-bold text-xl'>Dashboard</h1>
             <Folder />
         </aside>
