3b22154100c7228b1828d8b58a29fc7b3c7b5115
AdlerCastro <adlercastro01@gmail.com>
2024-07-26 16:34:25 -0300

    :wrench: chore: Configurações db

---
+++ src/utils/database/db.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const dbName = 'Dashboard'
+const MONGO_URL = `mongodb+srv://adlercastro01:${process.env.DB_PWD}@dashboard.mojraqz.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Dashboard`
+
+export const Connect = async () => {
+    try {
+        await mongoose.connect(MONGO_URL)
+        console.log("MongoDB conectado")
+    } catch (error) {
+        throw new Error(`Erro ao conectar ao MongoDB! Status: ${error}`)
+    }
+}
