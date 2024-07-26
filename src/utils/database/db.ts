a120cb85e2a8cdf72de2edeaa3626a11adf04cad
AdlerCastro <adlercastro01@gmail.com>
2024-07-26 16:52:26 -0300

    :recycle: refactor: Melhorando importação

--- src/utils/database/db.ts
+++ src/utils/database/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const dbName = 'Dashboard'
 const MONGO_URL = `mongodb+srv://adlercastro01:${process.env.DB_PWD}@dashboard.mojraqz.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Dashboard`
 
-export const Connect = async () => {
+const Connect = async () => {
     try {
         await mongoose.connect(MONGO_URL)
         console.log("MongoDB conectado")
@@ -11,3 +11,5 @@ export const Connect = async () => {
         throw new Error(`Erro ao conectar ao MongoDB! Status: ${error}`)
     }
 }
+
+export default Connect
