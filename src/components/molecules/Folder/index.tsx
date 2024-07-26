d9fb2e5518252aec633be5e9208ce7c718af02bc
AdlerCastro <adlercastro01@gmail.com>
2024-07-26 05:41:11 -0300

    :lipstick: feat: Estilização do componente de pastas para acesso as pages da dashboard

--- src/components/molecules/Folder/index.tsx
+++ src/components/molecules/Folder/index.tsx
@@ -5,36 +5,77 @@ import React, { useState } from 'react'
 
 export default function Folder() {
 
-    const [open, setOpen] = useState(false)
-    console.log(open)
+    const [openGeral, setOpenGeral] = useState(false)
+    const [openFerramentas, setOpenFerramentas] = useState(false)
+    const [openOpcional, setOpenOpcional] = useState(false)
+
+    const toggleGeral = () => {
+        setOpenGeral(!openGeral)
+    }
+
+    const toggleFerramentas = () => {
+        setOpenFerramentas(!openFerramentas)
+    }
+
+    const toggleOpcional = () => {
+        setOpenOpcional(!openOpcional)
+    }
 
     return (
-        <div className='flex flex-col gap-1'>
-            <nav className='relative items-center'>
-                <input type="checkbox" name="checkbox" id="checkbox" className="hidden" onClick={() => setOpen(!open)} />
-                <div className='relative'>
-                    <label htmlFor="checkbox" className='font-semibold text-base cursor-pointer'>
-                        Geral
-                    </label>
-                    <ul className={`absolute transition-all duration-100 list-none flex flex-col gap-y-2 bg-zinc-700 text-center ${open ? 'top-6 visible' : '-top-full hidden'}`}>
-                        <li><Button className='font-normal w-full'>aaa</Button ></li>
-                        <li><Button className='font-normal w-full'>bbb</Button ></li>
-                        <li><Button className='font-normal w-full'>ccc</Button ></li>
-                    </ul>
-                </div>
+        <div className='flex flex-1 flex-col justify-around'>
+            {/* Geral */}
+            <nav className='items-center'>
+                <input type="checkbox" name="checkbox" id="checkbox-geral" className="hidden" onClick={toggleGeral} />
+                <label htmlFor="checkbox-geral" className='font-semibold text-base cursor-pointer flex flex-row gap-2 items-center'>
+                    <span className={` w-5 border-solid items-center
+                    before:w-[13px] before:mt-1 before:h-[3px] before:relative before:block dark:before:bg-white before:bg-black
+                    after:w-[13px] after:mt-1 after:h-[3px] after:relative after:block dark:after:bg-white after:bg-black
+                        ${openGeral ? ' after:rotate-[225deg] before:rotate-[45deg]' : 'after:rotate-[-45deg] before:rotate-[45deg]'}`
+                    }></span> Geral
+                </label>
+                <ul className={` transition-all duration-300 list-none overflow-hidden ${openGeral ? 'h-[140px]' : 'h-0'}`}>
+                    <div className='transition-all duration-300 list-none flex flex-col gap-y-2 p-1 bg-transparent text-center '>
+                        <li><Button className='font-normal'>aaa</Button ></li>
+                        <li><Button className='font-normal'>bbb</Button ></li>
+                        <li><Button className='font-normal'>ccc</Button ></li>
+                    </div>
+                </ul>
             </nav>
-            <nav className='relative items-center'>
-                <input type="checkbox" name="checkbox" id="checkbox" className="hidden" onClick={() => setOpen(!open)} />
-                <div className='relative'>
-                    <label htmlFor="checkbox" className='font-semibold text-base cursor-pointer'>
-                        Geral
-                    </label>
-                    <ul className={`absolute transition-all duration-100 list-none flex flex-col gap-y-2 bg-zinc-700 text-center ${open ? 'top-6 visible' : '-top-full hidden'}`}>
-                        <li><Button className='font-normal w-full'>aaa</Button ></li>
-                        <li><Button className='font-normal w-full'>bbb</Button ></li>
-                        <li><Button className='font-normal w-full'>ccc</Button ></li>
-                    </ul>
-                </div>
+            {/* Ferramentas */}
+            <nav className='items-center'>
+                <input type="checkbox" name="checkbox" id="checkbox-ferramentas" className="hidden" onClick={toggleFerramentas} />
+                <label htmlFor="checkbox-ferramentas" className='font-semibold text-base cursor-pointer flex flex-row gap-2 items-center'>
+                    <span className={` w-5 border-solid items-center
+                    before:w-[13px] before:mt-1 before:h-[3px] before:relative before:block dark:before:bg-white before:bg-black
+                    after:w-[13px] after:mt-1 after:h-[3px] after:relative after:block dark:after:bg-white after:bg-black
+                        ${openFerramentas ? ' after:rotate-[225deg] before:rotate-[45deg]' : 'after:rotate-[-45deg] before:rotate-[45deg]'}`
+                    }></span> Ferramentas
+                </label>
+                <ul className={` transition-all duration-300 list-none overflow-hidden ${openFerramentas ? 'h-[140px]' : 'h-0'}`}>
+                    <div className='transition-all duration-300 list-none flex flex-col gap-y-2 p-1 bg-transparent text-center '>
+                        <li><Button className='font-normal'>aaa</Button ></li>
+                        <li><Button className='font-normal'>bbb</Button ></li>
+                        <li><Button className='font-normal'>ccc</Button ></li>
+                    </div>
+                </ul>
+            </nav>
+            {/* Opcional */}
+            <nav className='items-center'>
+                <input type="checkbox" name="checkbox" id="checkbox-opcional" className="hidden" onClick={toggleOpcional} />
+                <label htmlFor="checkbox-opcional" className='font-semibold text-base cursor-pointer flex flex-row gap-2 items-center'>
+                    <span className={` w-5 border-solid items-center
+                    before:w-[13px] before:mt-1 before:h-[3px] before:relative before:block dark:before:bg-white before:bg-black
+                    after:w-[13px] after:mt-1 after:h-[3px] after:relative after:block dark:after:bg-white after:bg-black
+                        ${openOpcional ? ' after:rotate-[225deg] before:rotate-[45deg]' : 'after:rotate-[-45deg] before:rotate-[45deg]'}`
+                    }></span> Opcional
+                </label>
+                <ul className={` transition-all duration-300 list-none overflow-hidden ${openOpcional ? 'h-[140px]' : 'h-0'}`}>
+                    <div className='transition-all duration-300 list-none flex flex-col gap-y-2 p-1 bg-transparent text-center '>
+                        <li><Button className='font-normal'>aaa</Button ></li>
+                        <li><Button className='font-normal'>bbb</Button ></li>
+                        <li><Button className='font-normal'>ccc</Button ></li>
+                    </div>
+                </ul>
             </nav>
         </div>
     )
