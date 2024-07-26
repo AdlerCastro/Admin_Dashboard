4d4a7f72bbbd8866b1218486b56ec52121609eb5
AdlerCastro <adlercastro01@gmail.com>
2024-07-26 16:51:54 -0300

    :sparkles: feat: Visualização da página

---
+++ src/app/Register/stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Register from "./page";
+
+export default{
+    title: "Components/Pages/Register",
+    component: Register
+}as Meta<StoryObj>
+
+type Story = StoryObj
+
+export const Page:Story = {}
