7867f3e2cd07730f01ab78e65cc636f4cb4f344b
AdlerCastro <adlercastro01@gmail.com>
2024-07-28 02:09:46 -0300

    style: Renomeando função

--- src/app/(Acess)/layout.tsx
+++ src/app/(Acess)/layout.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/templates/Hero";
 
-export default function LoginLayout({ children }: { children: React.ReactNode }) {
+export default function AcessLayout({ children }: { children: React.ReactNode }) {
 
     return (
         <div className="flex flex-row w-full h-full">
