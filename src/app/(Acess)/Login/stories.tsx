907bd9bc1d393e9807f4bc804bb6b37e7793c797
AdlerCastro <adlercastro01@gmail.com>
2024-07-28 02:10:19 -0300

    sparkles: style: Visualização no storybook

---
+++ src/app/(Acess)/Login/stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Login from "./page";
+
+export default{
+    title: "Components/Pages/Login",
+    component: Login
+}as Meta<StoryObj>
+
+type Story = StoryObj
+
+export const Page:Story = {}
