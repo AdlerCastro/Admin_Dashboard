3a65712b3d6f47d1b3042429efad1c44db7166cb
AdlerCastro <adlercastro01@gmail.com>
2024-07-28 02:33:21 -0300

    :sparkles: style: Visualização no storybook

---
+++ src/components/templates/Login/stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Login from ".";
+
+export default {
+    tilte: "Components/Templates/Login",
+    component: Login
+} as Meta<StoryObj>
+
+type Story = StoryObj
+
+export const Page: Story = {}
---
+++ src/components/templates/Register/stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Register from ".";
+
+export default {
+    tilte: "Components/Templates/Register",
+    component: Register
+} as Meta<StoryObj>
+
+type Story = StoryObj
+
+export const Page: Story = {}
