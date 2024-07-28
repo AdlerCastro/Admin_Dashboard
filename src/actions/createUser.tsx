5641bf601ebda5488df30035873e6e05c5ce367b
AdlerCastro <adlercastro01@gmail.com>
2024-07-28 02:05:13 -0300

    :construction: Futuramente utilizar no useMutation das credenciais

---
+++ src/actions/createUser.tsx
@@ -0,0 +1,17 @@
+export default async function createUser() {
+    try {
+        const res = await fetch("/api/register")
+        
+        if (res.status === 409) {
+            return console.log("*Email já cadastrado*")
+        }
+
+        if (res.status === 201) {
+            return console.log("*Email cadastrado com sucesso*")
+        }
+    }
+    catch (err) {
+        console.log(`Error, tente novamente ${err}`)
+    }
+
+}
