5b7e54de8fc9315dff43fa71e2083fff5c849f64
AdlerCastro <adlercastro01@gmail.com>
2024-07-28 02:08:10 -0300

    style: Removendo componente sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,6 @@ export default async function RootLayout({
       <body className={poppins.className}>
         <ReactQueryProvier>
           <SessionProvider session={session}>
-            <SideBar />
             <main>
               {children}
             </main>
