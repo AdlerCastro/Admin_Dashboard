e9387f6fc069c5e57db1a2530a29533d3e1f592d
AdlerCastro <adlercastro01@gmail.com>
2024-07-29 17:45:00 -0300

    :sparkles: feat: Página de Loading

---
+++ src/components/templates/Loading/index.tsx
@@ -0,0 +1,7 @@
+export default function LoadingPage() {
+    return (
+        <div className="flex w-full h-full items-center justify-center">
+            <h1 className="font-semibold text-4xl animate-ping duration-75">Carregando...</h1>
+        </div>
+    )
+}
