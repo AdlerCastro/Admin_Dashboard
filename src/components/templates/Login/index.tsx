76a6b70b378196da9590cfd601c6291096915622
AdlerCastro <adlercastro01@gmail.com>
2024-07-29 16:50:49 -0300

    :lipstick: style: Ajustando a estilização da page

--- src/components/templates/Login/index.tsx
+++ src/components/templates/Login/index.tsx
@@ -34,29 +34,29 @@ export default function LoginPage() {
   }
 
   return (
-    <div className="w-[400px] p-5 rounded-s-lg flex flex-col dark:bg-zinc-800">
-      <h1 className="text-6xl text-center mt-5">Bem-vindo a dashboard</h1>
-      <div className="flex flex-col h-full items-center justify-center -mt-[50%] gap-10">
-        <form onSubmit={handleSubmit} className="flex flex-col gap-10">
-          <label>
+    <div className="lg:w-[500px] p-5 px-8 flex flex-col rounded-lg dark:bg-zinc-800 gap-36">
+      <h2 className="text-6xl text-center mt-5">Bem-vindo a dashboard</h2>
+      <div className="flex flex-col items-center justify-center gap-10">
+        <form onSubmit={handleSubmit} className="w-full flex flex-col gap-10">
+          <label className='ml-8'>
             <h3 className="mb-2">Digite seu email</h3>
             <input
               type="email"
               name="email"
               placeholder="fulano@xxx.com"
-              className="text-black"
+              className=" text-black w-full"
             />
           </label>
-          <label>
+          <label className='ml-8'>
             <h3 className="mb-2">Digite sua senha</h3>
             <input
               type="password"
               name="password"
               placeholder="fulano123"
-              className="text-black"
+              className="text-black w-full"
             />
           </label>
-          <Button className="dark:bg-zinc-200 dark:text-black dark:hover:bg-zinc-300 dark:hover:text-black" type="submit">Acessar</Button>
+          <Button className="self-center dark:bg-zinc-200 dark:text-black dark:hover:bg-zinc-300 dark:hover:text-black" type="submit">Acessar</Button>
         </form>
         <Link href="/Register" className="hover:text-zinc-300">Não tem uma conta? Cadastre-se</Link>
         <p>{error}</p>
