186cdce923568e0c6a63c292db6be4a7f969ff7d
AdlerCastro <adlercastro01@gmail.com>
2024-07-29 16:44:59 -0300

    :lipstick: style: Animação na tela de Loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading(){
     return(
         <div className="flex w-full h-full items-center justify-center">
-            <h1>Carregando...</h1>
+            <h1 className="font-semibold text-4xl animate-ping duration-75">Carregando...</h1>
         </div>
     )
 }
