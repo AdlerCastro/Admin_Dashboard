e5a07f358158b5c13bcfdb8e46a6f58a07a3fcbd
AdlerCastro <adlercastro01@gmail.com>
2024-07-29 16:47:26 -0300

    :lipstick: style: Estilizando o componente Hero

--- src/components/templates/Hero/index.tsx
+++ src/components/templates/Hero/index.tsx
@@ -1,7 +1,8 @@
 export default function Hero() {
     return (
-        <div className="flex-1">
-            <h1>Hero</h1>
+        <div className="w-full h-full flex flex-col gap-3 items-center text-center justify-center bg-gradient-to-br from-zinc-900 to-zinc-500 text-white">
+            <h1 className="font-semibold text-5xl md:text-7xl">Dashboard de Administração</h1>
+            <h3>Gerenciamento Eficiente de Recursos e Operações</h3>
         </div>
     )
 }
