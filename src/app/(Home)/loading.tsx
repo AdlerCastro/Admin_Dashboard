31337fb515d392d3bfc7a384b8cc838290f524d8
AdlerCastro <adlercastro01@gmail.com>
2024-07-30 15:47:33 -0300

    :sparkles: feat: Loading da (Home)

---
+++ src/app/(Home)/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading(){
+    return(
+        <div className="flex items-center justify-center">
+            <h1 className="font-semibold text-sm animate-ping duration-1000">Carregando...</h1>
+        </div>
+    )
+}
