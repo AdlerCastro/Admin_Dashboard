f0e03f7ab877f764410bedb9e864c92420e1a719
AdlerCastro <adlercastro01@gmail.com>
2024-07-30 15:50:37 -0300

    :lipstick: style: Ajustes na estilização do texto

--- src/components/templates/Loading/index.tsx
+++ src/components/templates/Loading/index.tsx
@@ -1,7 +1,7 @@
 export default function LoadingPage() {
     return (
-        <div className="flex w-full h-full items-center justify-center">
-            <h1 className="font-semibold text-4xl animate-ping duration-75">Carregando...</h1>
+        <div className="flex w-full h-full items-center justify-center bg-zinc-900 text-zinc-50">
+            <h1 className="font-semibold text-4xl animate-ping duration-1000">Carregando...</h1>
         </div>
     )
 }
