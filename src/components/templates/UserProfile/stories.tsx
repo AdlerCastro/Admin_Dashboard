5e3cb1d0986841a45f99e948a2706efdf74d6075
AdlerCastro <adlercastro01@gmail.com>
2024-08-01 17:39:38 -0300

    :sparkles: feat: Visualização no storybook

---
+++ src/components/templates/UserProfile/stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import UserProfilePage from ".";
+
+export default {
+    tilte: "Components/Templates/UserProfile",
+    component: UserProfilePage
+} as Meta<StoryObj>
+
+type Story = StoryObj
+
+export const Page: Story = {}
