7597de916f81a35190ac09e8ed3c7f86cf81be8d
AdlerCastro <adlercastro01@gmail.com>
2024-08-06 16:18:38 -0300

    :sparkles: feat: Header da Lista

---
+++ src/components/molecules/List/Header/index.tsx
@@ -0,0 +1,23 @@
+interface Props {
+  tasksCounter: number
+  checkedTasksCounter: number
+}
+
+export function ListHeader({ tasksCounter, checkedTasksCounter }: Props) {
+  return (
+    <header className="flex flex-row justify-between items-center my-6 border-b-2 border-zinc-700 pb-4">
+      <aside className="flex flex-row gap-3">
+        <p>Tarefas criadas</p>
+        <span>{tasksCounter}</span>
+      </aside>
+      <aside className="flex flex-row gap-3">
+        <p>Concluídas</p>
+        <span>
+          {tasksCounter === 0
+            ? tasksCounter
+            : `${checkedTasksCounter} de ${tasksCounter}`}
+        </span>
+      </aside>
+    </header>
+  )
+}
