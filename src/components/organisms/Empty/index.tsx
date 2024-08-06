e2ee4c4bbb129a6469c76a07042d25b5c2eb2196
AdlerCastro <adlercastro01@gmail.com>
2024-08-06 16:19:20 -0300

    :sparkles: feat: Lista vazia

---
+++ src/components/organisms/Empty/index.tsx
@@ -0,0 +1,10 @@
+export function Empty() {
+  return (
+    <div className="w-full">
+      <p className="flex flex-col items-center">
+        <strong>Você ainda não tem tarefas cadastradas</strong>
+        Crie tarefas e organize seus itens a fazer
+      </p>
+    </div>
+  )
+}
